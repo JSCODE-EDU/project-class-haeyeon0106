aac5fe035c40715a298880994879905f993afc75
haeyeon0106 <yeon99016@gmail.com>
2023-05-11 22:13:01 +0900

    refactor: 도메인 수정

--- src/main/java/com/example/ProjectClass/domain/Post.java
+++ src/main/java/com/example/ProjectClass/domain/Post.java
@@ -1,16 +1,13 @@
 package com.example.ProjectClass.domain;
 
 import com.example.ProjectClass.dto.PostUpdateDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Entity
 @Table(name = "post")
@@ -23,6 +20,7 @@ public class Post extends BaseTimeEntity{
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
+    @Lob
     private String contents;
 
     public void update(PostUpdateDto updateDto){
