d569d8687f9c146e84330213b156c2b755072609
haeyeon0106 <yeon99016@gmail.com>
2023-05-25 14:18:24 +0900

    feat: 특정 게시글 조회 시 댓글 조회 구현

--- src/main/java/com/example/ProjectClass/dto/PostResponseDto.java
+++ src/main/java/com/example/ProjectClass/dto/PostResponseDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -19,10 +21,14 @@ public class PostResponseDto {
     @ApiModelProperty(example = "게시글 생성시간")
     private String createdAt;
 
+    @ApiModelProperty(example = "게시글에 작성된 모든 댓글")
+    private List<CommentResponseDto> comments;
+
     public PostResponseDto(Post entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.contents = entity.getContents();
         this.createdAt = entity.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.comments = entity.getComments().stream().map(CommentResponseDto::new).collect(Collectors.toList());
     }
 }
