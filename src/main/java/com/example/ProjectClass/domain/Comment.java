4ba5bb871542d0269d73c6bfead60e31056ea6f9
haeyeon0106 <yeon99016@gmail.com>
2023-05-29 11:01:50 +0900

    refactor: 지연로딩 추가

--- src/main/java/com/example/ProjectClass/domain/Comment.java
+++ src/main/java/com/example/ProjectClass/domain/Comment.java
@@ -20,11 +20,11 @@ public class Comment extends BaseTimeEntity{
     @Column
     private String contents;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
